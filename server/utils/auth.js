ff960aa2894cd7f2db18334d110d0720530cd846
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-08-05 16:09:06 -0500

    fixing authjs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,12 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
@@ -16,24 +22,19 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    } catch (err){
       console.log('Invalid token');
+      console.log('error:', err)
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    return req
+  } 
 };
