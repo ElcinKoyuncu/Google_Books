7f95f8782dc32b857524c2021ffea9ca4b18308e
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-08-05 15:30:35 -0500

    fixing serverjs

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -11,10 +10,17 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static("client/build"));
 }
 
 app.use(routes);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
